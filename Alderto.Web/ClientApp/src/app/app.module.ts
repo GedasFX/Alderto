8a865820a0370f57ad3b366ed3dac5ab4ae63186
Gediminas <8672277+GedasFX@users.noreply.github.com>
2019-07-06 20:14:17 +0300

    PreBig change again

--- Alderto.Web/ClientApp/src/app/app.module.ts
+++ Alderto.Web/ClientApp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
