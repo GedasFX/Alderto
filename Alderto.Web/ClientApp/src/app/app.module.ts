28ae849f4b5fddbde01cfa366f0e68e8c04bc931
Gediminas <8672277+GedasFX@users.noreply.github.com>
2019-07-06 21:14:31 +0300

    Add communication with .NET core and in turn - BOT

--- Alderto.Web/ClientApp/src/app/app.module.ts
+++ Alderto.Web/ClientApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; 
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -54,7 +55,8 @@ import { ChartsModule } from 'ng2-charts';
     PerfectScrollbarModule,
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
-    ChartsModule
+    ChartsModule,
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
--- Alderto.Web/ClientApp/src/app/views/base/tables.component.ts
+++ Alderto.Web/ClientApp/src/app/views/base/tables.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   templateUrl: 'tables.component.html'
 })
+
 export class TablesComponent {
+  forecasts: IWeatherForecast[];
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    http.get<IWeatherForecast[]>(window.location.origin + '/api/SampleData/WeatherForecasts').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+}
 
+interface IWeatherForecast {
+  dateFormatted: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
 }
