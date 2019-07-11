d83319316bbaa107c4f9476ed61e85a4806c2c8b
Gediminas <8672277+GedasFX@users.noreply.github.com>
2019-07-11 20:10:07 +0300

    Default Layout

--- Alderto.Web/ClientApp/src/app/app.component.ts
+++ Alderto.Web/ClientApp/src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'body',
+  templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  title = 'AldertoWeb';
-}
+export class AppComponent { }
--- Alderto.Web/ClientApp/src/app/app.module.ts
+++ Alderto.Web/ClientApp/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AppHeaderModule, AppSidebarModule, AppFooterModule } from '@coreui/angular';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppHeaderModule,
+    AppSidebarModule,
+    AppFooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
