f9da89c2aa0aff0f2ea93f926f3b99663ea71c31
Gediminas <8672277+GedasFX@users.noreply.github.com>
2019-07-21 21:58:54 +0300

    Add configuration for other aspects.

--- Alderto.Bot/Modules/GuildPreferencesModule.cs
+++ Alderto.Bot/Modules/GuildPreferencesModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Alderto.Bot.Extensions;
 using Alderto.Bot.Services;
 using Discord.Commands;
@@ -19,10 +20,31 @@ namespace Alderto.Bot.Modules
             }
 
             [Command("Prefix")]
-            public async Task GetPrefix()
+            public async Task Prefix()
             {
                 var pref = await _guildPreferencesManager.GetPreferencesAsync(Context.Guild.Id);
                 await this.ReplySuccessEmbedAsync($"Prefix: **{pref.Prefix}**");
+            }
+
+            [Command("CurrencySymbol"), Alias("CS")]
+            public async Task CurrencySymbol()
+            {
+                var pref = await _guildPreferencesManager.GetPreferencesAsync(Context.Guild.Id);
+                await this.ReplySuccessEmbedAsync($"Currency Symbol: **{pref.CurrencySymbol}**");
+            }
+
+            [Command("TimelyCooldown"), Alias("TimelyCD", "TCD")]
+            public async Task TimelyCooldown()
+            {
+                var pref = await _guildPreferencesManager.GetPreferencesAsync(Context.Guild.Id);
+                await this.ReplySuccessEmbedAsync($"Timely Cooldown: **{TimeSpan.FromSeconds(pref.TimelyCooldown)}**");
+            }
+
+            [Command("TimelyRewardQuantity"), Alias("TimelyRQ", "TRQ")]
+            public async Task TimelyRewardQuantity()
+            {
+                var pref = await _guildPreferencesManager.GetPreferencesAsync(Context.Guild.Id);
+                await this.ReplySuccessEmbedAsync($"Timely Reward Quantity: **{pref.TimelyRewardQuantity}**");
             }
         }
 
@@ -37,11 +59,31 @@ namespace Alderto.Bot.Modules
             }
 
             [Command("Prefix")]
-            public async Task SetPrefix(string prefix)
+            public async Task Prefix(string prefix)
             {
-                var guildId = Context.Guild.Id;
-                await _guildPreferencesManager.UpdatePreferencesAsync(guildId, pref => pref.Prefix = prefix);
+                await _guildPreferencesManager.UpdatePreferencesAsync(Context.Guild.Id, pref => pref.Prefix = prefix);
                 await this.ReplySuccessEmbedAsync($"Prefix was changed to: **{prefix}**");
+            }
+
+            [Command("CurrencySymbol"), Alias("CS")]
+            public async Task CurrencySymbol(string currencySymbol)
+            {
+                await _guildPreferencesManager.UpdatePreferencesAsync(Context.Guild.Id, configuration => configuration.CurrencySymbol = currencySymbol);
+                await this.ReplySuccessEmbedAsync($"Currency Symbol was changed to: **{currencySymbol}**");
+            }
+
+            [Command("TimelyCooldown"), Alias("TimelyCD", "TCD")]
+            public async Task TimelyCooldown(int cooldown)
+            {
+                await _guildPreferencesManager.UpdatePreferencesAsync(Context.Guild.Id, configuration => configuration.TimelyCooldown = cooldown);
+                await this.ReplySuccessEmbedAsync($"Timely Cooldown was changed to: **{TimeSpan.FromSeconds(cooldown)}**");
+            }
+
+            [Command("TimelyRewardQuantity"), Alias("TimelyRQ", "TRQ")]
+            public async Task TimelyRewardQuantity(int quantity)
+            {
+                await _guildPreferencesManager.UpdatePreferencesAsync(Context.Guild.Id, configuration => configuration.TimelyRewardQuantity = quantity);
+                await this.ReplySuccessEmbedAsync($"Timely Reward Quantity was changed to: **{quantity}**");
             }
         }
     }
