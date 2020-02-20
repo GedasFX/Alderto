90eacfdc6a05002e29e08f56c4feadfb60df1357
Gediminas <8672277+GedasFX@users.noreply.github.com>
2020-02-20 12:31:26 +0200

    add auth server

---
+++ Alderto.Web.Auth/Extensions/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alderto.Web.Auth.Extensions
+{
+    public class DependencyInjection
+    {
+    }
+}
