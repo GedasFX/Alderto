8d933f4e2dd0e3c5b50c8aa69a382d85638a0d01
Gediminas <8672277+GedasFX@users.noreply.github.com>
2020-05-01 11:33:34 +0300

    documentation

--- Alderto.Bot/Modules/HelpModule.cs
+++ Alderto.Bot/Modules/HelpModule.cs
@@ -7,24 +7,11 @@ namespace Alderto.Bot.Modules
     [Group("Help")]
     public class HelpModule : ModuleBase<SocketCommandContext>
     {
-        private readonly CommandService _commands;
-
-        public HelpModule(CommandService commands)
-        {
-            _commands = commands;
-        }
-
         [Command]
         [Summary("Shows help menu")]
         public async Task Help()
         {
-            await this.ReplyEmbedAsync(extra: builder =>
-            {
-                foreach (var command in _commands.Commands)
-                {
-                    builder.AddField(command.Name, command.Summary);
-                }
-            });
+            await this.ReplyEmbedAsync("For command reference visit https://alderto.com/documentation");
         }
     }
 }
